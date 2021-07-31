0cc21f6608de8e6aca55bdd62eba34acd4d9b249
Giordan <garredondop@uni.pe>
2021-07-30 22:30:47 -0500

    home
    Co-Authored-By: Billy Arredondo <22175195+gubiarpa@users.noreply.github.com>

---
+++ src/firstApplication/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace firstApplication
+{
+    
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hi, I'm a program!");
+            Circle myCircle; // creacion de un objeto tipo circulo
+
+            myCircle = new Circle(); // instanciamos la clase circulo
+
+            Console.WriteLine( myCircle.Area(4) );
+
+        }
+    }
+    class Circle
+    {
+        private const double pi = 3.14159;
+        public double Area(int radius)
+        {
+            return Math.Pow(radius, 2) * pi;
+        }
+    }
+
+}
+
